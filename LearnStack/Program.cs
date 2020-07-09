6aab20cae3977636043d6425b46e82a8f2932ccd
marco villanueva <villanuevamarco98@gmail.com>
2020-07-08 20:46:56 -0600

    Add project files.

---
+++ LearnStack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearnStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //last in first out
+            Stack<String> pancakes = new Stack<string>();
+
+            pancakes.Push("first pancake made");
+            pancakes.Push("second pancake made");
+            pancakes.Push("third pancake made");
+
+            foreach (var pancake in pancakes)
+            {
+                Console.WriteLine(pancake);            }
+
+        }
+
+
+
+
+    }
+}
